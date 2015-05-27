3cbfef31fbd219ce0249f825b5a804feee4db9ac
cnmillar <cnmillar@gmail.com>
2015-05-27 18:05:57 +0000

    Debugged code for debugging exercise

--- debug01.rb
+++ debug01.rb
@@ -1,4 +1,12 @@
-list = {'yvr' => 'Vancouver', 'yba' => 'Banff', 'yyz' => 'Toronto', 'yxx' => 'Abbotsford', 'ybw' => 'Calgary'}
+list = {'yvr' => 'Vancouver', 
+		'yba' => 'Banff', 
+		'yyz' => 'Toronto', 
+		'yxx' => 'Abbotsford', 
+		'ybw' => 'Calgary'
+	}
 
 # Why is it returning nil instead of first element of the list above
-p list[0]
+# p list[0]
+# Because this is a hash not an array. Need to refer to key.
+
+p list['yvr']
